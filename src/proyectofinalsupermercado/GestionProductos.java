8288f34fc5172e4498bd3aa39b0ecefce2579c6d
kevin <kevin@192.168.1.72>
2021-11-23 11:49:09 -0600

    commit x4

--- src/proyectofinalsupermercado/GestionProductos.java
+++ src/proyectofinalsupermercado/GestionProductos.java
@@ -29,11 +29,11 @@ public class GestionProductos extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 415, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 128, Short.MAX_VALUE)
         );
 
         pack();
